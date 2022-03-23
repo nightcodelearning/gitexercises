75f53d8356c189d7c28305531cb7a8e6bc1a4743
jerome <nigthcodelearning@gmail.com>
2022-03-22 21:06:39 -0500

    ultimo cambio reporte

--- Reporte.java
+++ Reporte.java
@@ -1,7 +1,36 @@
 public class Reporte{
 
+    private String cadena;
+    private int numero;
+    private String valor;
+    public Reporte(String cadena, int numero, String valor) {
+        this.cadena = cadena;
+        this.numero = numero;
+        this.valor = valor;
+    }
     public static void main(String[] args) {
         System.out.println("Hola Reporte");
         System.out.println("Hola Inés");
     }
+    public String getCadena() {
+        return cadena;
+    }
+    public void setCadena(String cadena) {
+        this.cadena = cadena;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getValor() {
+        return valor;
+    }
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+
+    
 }
